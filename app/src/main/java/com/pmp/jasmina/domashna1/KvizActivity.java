4afdd8d228a39e2aefc4b3da85c1e99addaa720d
Jasmina_K <jasminakukunoska@gmail.com>
2017-03-12 22:29:06 +0100

    Домашна 2 - прв commit

---
+++ app/src/main/java/com/pmp/jasmina/domashna1/KvizActivity.java
@@ -0,0 +1,17 @@
+package com.pmp.jasmina.domashna1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Jasmina on 12.03.2017.
+ */
+
+public class KvizActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kviz_layout);
+    }
+}
--- app/src/main/java/com/pmp/jasmina/domashna1/MainActivity.java
+++ app/src/main/java/com/pmp/jasmina/domashna1/MainActivity.java
@@ -1,7 +1,10 @@
 package com.pmp.jasmina.domashna1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void prikaziKviz(View view){
+        String button_text;
+        button_text = ((Button) view).getText().toString();
+        if (button_text.equals("Igraj kviz")){
+            Intent intent = new Intent(this, KvizActivity.class);
+            startActivity(intent);
+        }
+    }
+
+
 }
